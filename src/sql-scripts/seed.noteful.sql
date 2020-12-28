a782d58cd7b6aca53c5698ab97dc7e081d8cd47a
fschweikhardt <schweikhardt.web@gmail.com>
2020-12-28 13:40:24 -0500

    removed duplicates

--- src/sql-scripts/seed.noteful.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO folders_table (title) VALUES
-('Important')
-('Super')
-('Mega')
-
-INSERT INTO notes_table (note_name, modified, content, folderId) VALUES
-("Dogs", now(), "dog content", 1)
