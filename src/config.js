ad71a2f912357a7d016eae7dc0500eab9b36124f
fschweikhardt <schweikhardt.web@gmail.com>
2020-12-28 14:44:49 -0500

    changed .json

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 9000,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV || 'production',
     DATABASE_URL: process.env.DB_URL || 'postgresql://postgres:postgres@localhost/postgres',
     API_KEY: process.env.API_KEY
   }
