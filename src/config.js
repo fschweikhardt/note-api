e2770712a9574dcfad33148cc121416c3f39349e
fschweikhardt <schweikhardt.web@gmail.com>
2020-12-28 12:11:25 -0500

    hide keys

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     PORT: process.env.PORT || 9000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres:postgres@localhost/postgres',
-    API_TOKEN: process.env.API_TOKEN || "123456789"
+    API_KEY: process.env.API_KEY
   }
