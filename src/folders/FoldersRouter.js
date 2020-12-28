567a3457bc7c6d5c77c6f0795c11dc828e4a7872
fschweikhardt <schweikhardt.web@gmail.com>
2020-12-28 11:01:48 -0500

    tested all endpoints working

--- src/folders/FoldersRouter.js
+++ src/folders/FoldersRouter.js
@@ -14,30 +14,9 @@ FoldersRouter
             })
             .catch(next)
         })
-
-// NotefulRouter
-//     .route('/notes/:noteId')
-//     .get((req,res,next) => {
-//         const { noteId } = req.params
-//         NotefulService.getNoteById(req.app.get('db'), noteId)
-//             .then(data => {
-//                 res.json(data)
-//                 console.log(data)
-//             })
-//             .catch(next)
-
-//     })
-//     .delete((req,res,next) => {
-//         const { noteId } = req.params
-//         NotefulService.deleteNote(req.app.get('db'), noteId )
-//             .then(console.log('note deleted'))
-//             .then(res.status(204).end())
-//             .catch(next)
-//     })
     
-
 FoldersRouter
-    .route('api/folders/:folderid')
+    .route('/api/folders/:folderid')
     .get((req,res,next) => {
         const { folderid } = req.params
         FoldersService.getNotesByFolderId(req.app.get('db'), folderid)
@@ -47,34 +26,14 @@ FoldersRouter
             })
             .catch(next)
     })
-    //.delete
-
-
-// NotefulRouter
-//     .route('/add-note')
-//     .post(bodyParser, (req, res, next) => {
-//         //let modified = now()
-//         const { note_name, modified, content, folderid } = req.body
-//         const newNote = { note_name, modified, content, folderid }
-//         NotefulService.addNote(req.app.get('db'), newNote)
-//             .then(data => {
-//                 res.json(data)
-//                 console.log(data)
-//             })
-//             .catch(next)
-//     })
 
 FoldersRouter
-    .route('/add-folder')
+    .route('/api/add-folder')
     .post(bodyParser, (req, res, next) => {
         //let modified = now()
         const { title } = req.body
         const newFolder = { title }
         FoldersService.addFolder(req.app.get('db'), newFolder)
-        //     .then(data => {
-        //         //res.json(data)
-        //         //console.log(data)
-        //    })
             .catch(next)
     })
 
--- src/folders/FoldersService.js
+++ src/folders/FoldersService.js
@@ -1,34 +1,11 @@
 const FoldersService = { 
 
-    // getAllData(knex) {
-    //     //return knex.select('*').from('folders_table').from('notes_table')
-    //     //return knex.join('folders_table', 'notes_table').select('*')
-    //     let notes = knex.select('*').from('notes_table')
-    //     let folders = knex.select('*').from('folders_table')
-    //     return notes.concat(folders)  
-    // },
-
-    // getAllNotes(knex) {
-    //     return knex.select('*').from('notes_table')
-    // },
-    // getNoteById(knex, id) {
-    //     return knex.select('*').from('notes_table').where('id', id).first()
-    // },
     getAllFolders(knex) {
         return knex.select('*').from('folders_table')
     },
-    getNotesByFolderId(knex, folderid) {
-        return knex.select('*').from('notes_table').where('folderid', folderid)
+    getNotesByFolderId(knex, id) {
+        return knex.select('*').from('folders_table').where('id', id).first()
     }, 
-    // deleteNote(knex, id) {
-    //     return knex.select('*').from('notes_table').where('id', id).delete()
-    // }, 
-    // addNote(knex, newNote) {
-    //     return knex.insert(newNote).into('notes_table').returning('*')
-    //         .then(rows => {
-    //             return rows[0] 
-    //         })
-    // }, 
     addFolder(knex, newFolder) {
         return knex.insert(newFolder).into('folders_table').returning('*')
             .then(rows => {
