a95814a951bc2b1e47d8ffcc8ba324aeab5d645b
fschweikhardt <schweikhardt.web@gmail.com>
2020-12-28 20:54:05 -0500

    added node engine

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 9000,
     NODE_ENV: process.env.NODE_ENV || 'production',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost/postgres',
+    DATABASE_URL: process.env.DATABASE_URL, //|| 'postgresql://postgres:postgres@localhost/postgres',
     API_KEY: process.env.API_KEY
   }
